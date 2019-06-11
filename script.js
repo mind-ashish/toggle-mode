4d36b7305b901223f64a141dea8f16a7f7137a7c
Aashish <aashish01official@gmail.com>
2019-06-11 22:57:07 +0530

    js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-var switch=document.getElementById("toggle-switch");
+var switch1=document.getElementById("toggle-switch");
 var b=document.getElementsByTagName("body");
 var body=b[0];
 var flag=0;
-var bColor=body.style.backgroundColor;
-var color=body.style.color;
+var bColor1="rgb(250, 246, 246)";
+var color1="black";
+var bColor2="rgba(0, 0, 0, 0.87)";
+var color2="white";
 
+function toggle(){
+    if(flag==0){
+        body.style.backgroundColor=bColor2;
+        body.style.color=color2;
+        flag=1;
+    }else{
+        body.style.backgroundColor=bColor1;
+        body.style.color=color1;
+        flag=0;
+    }
+}
+switch1.addEventListener('click',toggle);
