4c39e679f6c154ec2882048d3434a02d9e552b61
Aashish <aashish01official@gmail.com>
2019-06-11 22:39:20 +0530

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+var switch=document.getElementById("toggle-switch");
+var b=document.getElementsByTagName("body");
+var body=b[0];
+var flag=0;
+var bColor=body.style.backgroundColor;
+var color=body.style.color;
+
